67583568252b146207e11c59bdd84e0ae388b7be
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-04-25 17:01:54 +0800

    ask the user if they want to append more lines and if they answer y (yes), start again, and end the program otherwise

--- Diary entry.py
+++ Diary entry.py
@@ -1,9 +1,15 @@
-# ask user for input
 def entry():
+    # ask user for input
     diary_entry = input('Entry: ')
     # append input to text file
     life_update = open('mylife.txt', 'a')
     life_update.write(diary_entry)
-# ask if they want to input more into the text file
-# if yes, start again
-# if no, end program
+
+while True:
+    entry()
+    # ask if they want to input more into the text file
+    choice = input('Do you want to add more? (y/n) ')
+    # if yes, start again
+    # if no, end program
+    if choice == 'n':
+        break
