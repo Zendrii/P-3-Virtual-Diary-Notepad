bab09e4cc0704e6b7bd36bbe9a04643c765df698
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-04-25 16:55:25 +0800

    appending input to file

--- Diary entry.py
+++ Diary entry.py
@@ -1,7 +1,9 @@
 # ask user for input
 def entry():
     diary_entry = input('Entry: ')
-# append input to text file
+    # append input to text file
+    life_update = open('mylife.txt', 'a')
+    life_update.write(diary_entry)
 # ask if they want to input more into the text file
 # if yes, start again
 # if no, end program
