f81298f6ae27e2caf9fc2d65f7c5cb1d35236335
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-04-25 17:11:43 +0800

    designing and final adjustments

--- Diary entry.py
+++ Diary entry.py
@@ -1,15 +1,21 @@
+print('\033[92m='*158)
+
 def entry():
     # ask user for input
-    diary_entry = input('Entry: ')
+    diary_entry = input('\nEntry: ')
     # append input to text file
     life_update = open('mylife.txt', 'a')
-    life_update.write(diary_entry)
+    life_update.write(diary_entry + '\n')
 
 while True:
     entry()
     # ask if they want to input more into the text file
-    choice = input('Do you want to add more? (y/n) ')
+    print('\nWriting. . . . .')
+    import time
+    time.sleep(1)
+    choice = input('\nDo you want to add more? (y/n) ')
     # if yes, start again
+    print('\033[92m='*158)
     # if no, end program
     if choice == 'n':
         break
